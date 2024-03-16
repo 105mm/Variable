7eb7b426cbdd19450b64063f497c563a4e79f19f
105mm <king7532@knou.ac.kr>
2024-03-16 14:33:59 +0900

    Commit

---
+++ src/Variable02.java
@@ -0,0 +1,21 @@
+public class Variable02 {
+    public static void main(String[] args) {
+        // 변수에 값 할당하기
+        // 정수를 저장할 수 있는 변수 num1 선언
+
+        int num1;
+
+        // num1에 10을 저장
+        // '='(대입연산자)
+
+        num1 = 10;
+
+        // num1 = "java"; <- 오류
+
+        int num2;
+        num2 = 20;
+
+        System.out.println(num2);
+        System.out.println(num1);
+    }
+}
